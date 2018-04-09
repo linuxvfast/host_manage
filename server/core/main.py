2303db0f4392f6dd17da6d541a8d31393b6f2b3a
linuxvfast <2559811312@qq.com>
2018-04-09 23:20:25 +0800

    添加cmd操作

--- server/core/main.py
+++ server/core/main.py
@@ -4,9 +4,18 @@ import threading
 from conf import settings
 
 class Server(object):
-    pass
+    def batch_cmd(self,user_list):
+        '''执行命令'''
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=user_list[ip])
+
+    def batch_put(self,user_list):
+        '''上传文件'''
+        pass
 
     def interactive(self):
+        '''用户交互'''
         config = configparser.ConfigParser()
         config.read(settings.ACCOUNT_DIR)
         print('group1\t2台\ngroup2\t1台')
@@ -24,7 +33,19 @@ class Server(object):
                         print(key,' ',config[choice][key])
             else:
                 print('\033[41;1m输入错误,请重新输入！\033[0m')
+            while True:
+                print('可执行的操作'.center(30,'-'))
+                print('1\t执行命令\n2\t上传文件')
+                choice_option = input('>>>').strip()
+                if choice_option == '1':
+                    self.batch_cmd(config[choice])
+                elif choice_option == '2':
+                    self.batch_put(config[choice])
+                else:
+                    continue
+
 
 def run():
     d = Server()
     d.interactive()
+    d.user_operation()
